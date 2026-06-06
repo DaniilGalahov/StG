694d9b96db5ce775535ece3b11ed2c3c7131f955
Daniil Galahov <daniil.galahov@yandex.ru>
2026-06-06 12:53:50 +0300

    Added separated DataProcessor
    Signed-off-by: Daniil Galahov <daniil.galahov@yandex.ru>

---
+++ StG/DataProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using AESBridge;
+using StGBridge;
+
+namespace StG
+{
+    internal class DataProcessor
+    {
+        Byte[] Zip(Byte[] dataBytes)
+        {
+            MemoryStream dataStream = new MemoryStream();
+            using (dataStream)
+            {
+                dataStream.Write(dataBytes, 0, dataBytes.Length);
+                dataStream.Position = 0;
+            }
+
+            MemoryStream zippedDataStream = new MemoryStream();
+            using (zippedDataStream)
+            {
+                using (GZipStream compressor = new GZipStream(zippedDataStream, CompressionMode.Compress))
+                {
+                    dataStream.CopyTo(compressor);
+                }
+            }
+
+            Byte[] zippedDataBytes = zippedDataStream.ToArray();
+
+            dataStream.Close();
+            zippedDataStream.Close();
+
+            return zippedDataBytes;
+        }
+    }
+}
