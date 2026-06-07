36d8ab2b53e0cd017fc90e0a4b62b8a67f3227b2
Daniil Galahov <daniil.galahov@yandex.ru>
2026-06-07 23:22:42 +0300

    Created distributive maker script (still no solution for CI/CD)
    Signed-off-by: Daniil Galahov <daniil.galahov@yandex.ru>

---
+++ make.ps1
@@ -0,0 +1,25 @@
+# Removing previous distributive package
+Remove-Item ".\dist\*.*" -Force
+Remove-Item ".\dist" -Recurse -Force
+
+# Creating new distributive folder
+New-Item -Path ".\dist" -ItemType Directory
+
+# Copying StG distributives
+Copy-Item -Path ".\StG\bin\Release\StG.exe" -Destination ".\dist"
+Copy-Item -Path ".\StG\bin\Release\AESBridge.dll" -Destination ".\dist"
+Copy-Item -Path ".\StG\bin\Release\StGBridge.dll" -Destination ".\dist"
+
+# Copying OpenCV lib
+Copy-Item -Path "C:\opencv\build\x64\vc16\bin\opencv_world4120.dll" -Destination ".\dist"
+
+# Copying documentation
+Copy-Item -Path ".\docs\Readme.pdf" -Destination ".\dist"
+Copy-Item -Path ".\docs\License.pdf" -Destination ".\dist"
+Copy-Item -Path ".\OpenCV.txt" -Destination ".\dist"
+
+# Compress archive
+Compress-Archive -Path ".\dist\*" -Destination ".\dist\StG $(Get-Date -Format 'yyyy-MM-dd HH-mm-ss').zip"
+
+
+PAUSE
